20e4f315ba7be9a46fc4f52cafd47306b970ee41
Danuka <danukakoshitha2004@gmail.com>
2025-04-29 13:14:51 +0530

    add employee

--- src/main/java/org/example/service/EmployeeService.java
+++ src/main/java/org/example/service/EmployeeService.java
@@ -1,4 +1,9 @@
 package org.example.service;
 
+import org.example.dto.Employee;
+
 public interface EmployeeService {
+    boolean addEmployee(Employee employee);
+
+    boolean emailExists(String email);
 }
---
+++ src/main/java/org/example/util/Department.java
@@ -0,0 +1,5 @@
+package org.example.util;
+
+public enum Department {
+    HR,IT,FINANCE,OPERATIONS
+}
